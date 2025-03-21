d7b440dba09e7846f0bbf6ccd6f9a7be7da8d597
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-21 14:01:26 -0400

    feat: implement email sender service

--- src/main/java/dev/haymon/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/dev/haymon/jwtsecurity/JwtSecurityApplication.java
@@ -2,11 +2,12 @@ package dev.haymon.jwtsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class JwtSecurityApplication {
 
 	public static void main(String[] args) {
