fe673eb6e3d202e328db9cbff12868d606e906bc
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-22 00:23:53 -0400

    feat(auth): implement registration endpoint

---
+++ src/main/java/dev/haymon/jwtsecurity/auth/AuthenticationController.java
@@ -0,0 +1,26 @@
+package dev.haymon.jwtsecurity.auth;
+
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) throws MessagingException {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+}
