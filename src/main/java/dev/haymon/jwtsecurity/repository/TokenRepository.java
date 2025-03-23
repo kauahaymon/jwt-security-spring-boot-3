2e8b6cf8a04ad0ce3ff04371ca0e6ef2ba9e70e1
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-23 17:02:00 -0400

    feat(repository): implement find by token name

--- src/main/java/dev/haymon/jwtsecurity/repository/TokenRepository.java
+++ src/main/java/dev/haymon/jwtsecurity/repository/TokenRepository.java
@@ -3,5 +3,8 @@ package dev.haymon.jwtsecurity.repository;
 import dev.haymon.jwtsecurity.model.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
 }
