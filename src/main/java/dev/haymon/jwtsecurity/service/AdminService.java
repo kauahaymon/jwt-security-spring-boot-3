29f7a9a5ec8db6862ff5d17e944dc7aa487d2acc
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-27 00:47:07 -0400

    feat: implement user deletion service and deletion validator

--- src/main/java/dev/haymon/jwtsecurity/service/AdminService.java
+++ src/main/java/dev/haymon/jwtsecurity/service/AdminService.java
@@ -2,6 +2,7 @@ package dev.haymon.jwtsecurity.service;
 
 import dev.haymon.jwtsecurity.model.User;
 import dev.haymon.jwtsecurity.repository.UserRepository;
+import dev.haymon.jwtsecurity.service.validation.UserDeletionValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,9 +14,15 @@ import org.springframework.stereotype.Service;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final UserDeletionValidator deletionValidator;
 
     public Page<User> getUsers(Integer pageNumber, Integer pageSize) {
         Pageable userPage = PageRequest.of(pageNumber, pageSize);
         return userRepository.findAll(userPage);
+    }
+
+    public void delete(Integer id) {
+        deletionValidator.validate(id);
+        userRepository.deleteById(id);
     }
 }
