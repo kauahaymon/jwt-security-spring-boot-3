37f86d473b8b985378b0ddb95a695d41e3ec0838
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-27 18:32:01 -0400

    refactor: remove imports

--- src/main/java/dev/haymon/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/dev/haymon/jwtsecurity/JwtSecurityApplication.java
@@ -1,11 +1,7 @@
 package dev.haymon.jwtsecurity;
 
-import dev.haymon.jwtsecurity.model.Role;
-import dev.haymon.jwtsecurity.repository.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
