d5ecae1b482ccb149c94daaae51e1f0cd120aeb1
Kauã Haymon <kauahaymon@hotmail.com>
2025-04-10 23:01:06 -0400

    feat: migration order mapper to mapstruct

--- src/main/java/dev/haymon/jwtsecurity/controller/mapper/OrderMapper.java
+++ src/main/java/dev/haymon/jwtsecurity/controller/mapper/OrderMapper.java
@@ -1,32 +1,11 @@
 package dev.haymon.jwtsecurity.controller.mapper;
 
-import dev.haymon.jwtsecurity.controller.dto.order.OrderItemResponse;
 import dev.haymon.jwtsecurity.controller.dto.order.OrderResponse;
 import dev.haymon.jwtsecurity.model.Order;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-import java.util.List;
+@Mapper(componentModel = "spring", uses = {OrderItemMapper.class})
+public interface OrderMapper {
 
-@Component
-public class OrderMapper {
-
-    public OrderResponse toResponseDTO(Order order) {
-        List<OrderItemResponse> itemResponses = order.getItems()
-                .stream()
-                .map(orderItem -> OrderItemResponse.builder()
-                        .productId(orderItem.getId())
-                        .nome(orderItem.getProduct().getName())
-                        .quantity(orderItem.getQuantity())
-                        .unitPrice(orderItem.getUnitPrice())
-                        .subTotal(orderItem.getSubTotal())
-                        .build())
-                        .toList();
-
-        return OrderResponse.builder()
-                .id(order.getId())
-                .createdAt(order.getCreatedAt())
-                .total(order.getTotal())
-                .items(itemResponses)
-                .build();
-    }
+    OrderResponse toResponseDTO(Order order);
 }
